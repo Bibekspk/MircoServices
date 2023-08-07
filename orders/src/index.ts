f3c95fb24597e160a9704b2585e86077009294ab
Subash Poudel <subash.poudel@zegal.com>
2023-08-07 18:52:55 +0545

    order service

---
+++ orders/src/index.ts
@@ -0,0 +1,30 @@
+import { app } from "./app";
+import "./db-connect";
+import { natsWrapper } from "./nats-wrapper";
+
+const start = async () => {
+  if (!process.env.NATS_CLIENT_ID) throw new Error("client id is not defined");
+  if (!process.env.NATS_URL) throw new Error("nats uri is not defined");
+  if (!process.env.NATS_CLUSTER_ID)
+    throw new Error("cluster id is not defined");
+
+  try {
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed");
+      process.exit();
+    });
+    process.on("SIGINT", () => natsWrapper.client.close()); //signal interrupt
+    process.on("SIGTERM", () => natsWrapper.client.close()); //signal terminate
+  } catch (err) {}
+
+  app.listen(4000, () => {
+    console.log("listening on port 3000");
+  });
+};
+
+start();
---
+++ orders/src/routes/__tests__/show.test.ts
@@ -0,0 +1,28 @@
+import request from "supertest";
+import { app } from "../../app";
+import { Ticket } from "../../models/ticket";
+
+it("fetches the order", async () => {
+  const ticket = Ticket.build({ title: "concert", price: 40 });
+  await ticket.save();
+
+  const user = global.signin();
+  const { body: order } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  const { body: fetchedOrder } = await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set("Cookie", user)
+    .send()
+    .expect(200);
+
+  expect(fetchedOrder.id).toEqual(order.id);
+  await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set("Cookie", global.signin())
+    .send()
+    .expect(401);
+});
