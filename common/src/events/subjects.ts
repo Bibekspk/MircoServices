693a0803b29e2f938dca547ad507209e8cdef41b
bibek sapkota <bibeksp7@gmail.com>
2023-08-27 17:14:10 +0545

    updates

--- common/src/events/subjects.ts
+++ common/src/events/subjects.ts
@@ -4,4 +4,5 @@ export enum Subjects {
   OrderCreated = "order:created",
   OrderCancelled = "order:cancelled",
   ExpirationComplete = "order:expired",
+  PaymentCreated = "payment:created"
 }
--- common/src/index.ts
+++ common/src/index.ts
@@ -10,6 +10,8 @@ export * from "./middlewares/error-handler";
 export * from "./middlewares/require-auth";
 export * from "./middlewares/vaildate-request";
 
+export * from "./events/types/order-types";
+
 export * from "./events/base-listener";
 export * from "./events/base-publisher";
 export * from "./events/subjects";
@@ -17,6 +19,5 @@ export * from "./events/ticket-created-event";
 export * from "./events/ticket-updated-event";
 export * from "./events/order-cancelled-event";
 export * from "./events/order-created-event";
-
-export * from "./events/types/order-types";
 export * from "./events/expiration-complete-event";
+export * from './events/payment-created-event'
